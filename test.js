4c6de4b96fea13cff75a7123a72e073fbe90922c
unknown <dezaodoido@gmail.com>
2015-05-05 13:14:38 -0300

    fixed some bugs

--- test.js
+++ test.js
@@ -8,9 +8,10 @@ describe('jinx-compiler', function() {
 	var mainFile = 'test/app/flash/init.jinx';
 	it('should compile .jinx into .as and modules', function(done) {
 		var compiled = jinxCompiler(fs.readFileSync(mainFile));
-		
-		expect(compiled).to.contain('__jinx_require__(1)');
-		expect(compiled).to.not.contain('require (	"jinx-mempanel" )');
+		var contents = String(compiled.contents);
+		console.log(contents);
+		expect(contents).to.contain('__jinx_require__(1)');
+		expect(contents).to.not.contain('require (	"jinx-mempanel" )');
 		done();
 	});
 });
