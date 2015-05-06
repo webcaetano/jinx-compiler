d9f26f2e5fcc7305c87d210950d972d0c0e56b59
unknown <dezaodoido@gmail.com>
2015-05-06 10:30:02 -0300

    fixed include

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ describe('jinx-compiler', function() {
 	it('should compile .jinx into .as and modules', function(done) {
 		var compiled = jinxCompiler(fs.readFileSync(mainFile));
 		var contents = String(compiled.contents);
-		console.log(contents);
+		
 		expect(contents).to.contain('__jinx_require__(1)');
 		expect(contents).to.not.contain('require (	"jinx-mempanel" )');
 		done();
